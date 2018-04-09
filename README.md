2493adbab372e00a0226b9f80842d40f95de523f
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-04-09 08:32:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # avtale-robot
+
 Robot for our agreements service
+
+Retrieves unsigned agreements checks their status and produces jobs for status updates
+
+# License
+
+[MIT](LICENSE)
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/directories/done',
+  ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/directories/errors',
+  JOBS_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/jobs',
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
+  AGREEMENTS_SERVICE_URL: process.env.AGREEMENTS_SERVICE_URL || 'https://agreements.service.no',
+  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'status-robot',
+  PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
+  PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
+}
