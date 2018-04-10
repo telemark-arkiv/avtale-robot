b458e09cb1ab3f8e45f07b0ea3ec57f532a5e6a5
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-04-10 07:49:06 +0200

    Adds more basic methods

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const getUnsignedAgreements = require('./lib/steps/get-unsigned-agreements')
+const checkAgreementsStatus = require('./lib/steps/check-agreements-status')
+const generateUpdateJobs = require('./lib/steps/generate-update-jobs')
+const saveJobs = require('./lib/steps/save-jobs')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
 
 getUnsignedAgreements()
+  .then(checkAgreementsStatus)
+  .then(generateUpdateJobs)
+  .then(saveJobs)
   .then(data => {
     logger('info', ['index', 'finished'])
     process.exit(0)
